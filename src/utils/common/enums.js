ec02db108defa61a33e505b6c7c04166150c2a72
Floyden <floydenmonteiro@gmail.com>
2023-07-07 19:42:03 +0530

    Added models for many to many assoc between roles and user

---
+++ src/utils/common/enums.js
@@ -0,0 +1,10 @@
+const USER_ROLE_ENUMS = {
+  ADMIN: 'admin',
+  CUSTOMER: 'customer',
+  FLIGHT_COMPANY: 'flight_company',
+ 
+};
+
+module.exports = {
+  USER_ROLE_ENUMS
+};
--- src/utils/common/index.js
+++ src/utils/common/index.js
@@ -2,4 +2,5 @@ module.exports = {
   ErrorResponse: require('./error-response'),
   successResponse: require('./success-response'),
   Auth: require('./auth'),
+  Enums: require('./enums'),
 };
